47e29422727c17cf709a7cb719932d4d2564c66e
gitdigit <jennifer.kouassi@edu.ece.fr>
2026-02-17 00:30:52 +0100

    feat: footer and pokéapi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -17,17 +18,51 @@ export const metadata: Metadata = {
   description: "An app to list my drawings and animation (Procreate and Procreate Dreams)",
 };
 
-export default function RootLayout({
+async function getRandomPokemon() {
+  const randomId = Math.floor(Math.random() * 151) + 1;
+
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${randomId}`,
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+
+  return {
+    name: data.name,
+    image: data.sprites.front_default,
+  };
+}
+
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pokemon = await getRandomPokemon();
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <footer className="mt-32 py-10 text-center text-sm text-zinc-500 flex flex-col items-center gap-3">
+  <Image
+    src={pokemon.image}
+    alt={pokemon.name}
+    width={72}
+    height={72}
+    className="hover:scale-110 transition"
+  />
+
+  <p className="text-xs tracking-wide">
+    Powered by Jen and PokéAPI
+  </p>
+</footer>
+
       </body>
     </html>
   );
